21bdb3ee7c58c9bd881ecd354314fdd95b4b3b6e
bruno vicente <bruno.marvicente@gmail.com>
2024-07-30 19:49:01 +0100

    ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,22 @@
 #include <Arduino.h>
 #include <ESP32_CAN.h>
 #include <Adafruit_NeoPixel.h>
-
+#include <WiFi.h>
+#include <WiFiAP.h>
+#include <WebServer.h>
+#include <ESP2SOTA.h>
 
 #define LED_PIN 16
 #define NUM_OF_PINS 48
 
-#define BTN 34
-#define Accelaration 4 
-#define Skidpad 27
-#define Autocross 5
-#define Trackdrive 17
-#define EBS_test 33
-#define Inspection 23
-
-
 unsigned long tim = 0;
 unsigned long timeout = 0;
-unsigned long timeout_interrupt = 0;
-volatile int flag_interrupt = 0;
+
+
+const char* ssid = "ASSI_LART_T24";
+const char* password = "T24E_aut";
+WebServer server(80);
+
 
 TWAI_Interface CAN1(1000, 21, 22);
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_OF_PINS, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -26,38 +24,54 @@ int flag = 0;
 int luz_state_3 = 0;
 int luz_state_4 = 0;
 int emergenencia = 0;
-volatile int missao = 0;
+int missao = 0;
 uint32_t CAN_ID = 0;
 uint8_t var[8];
 
 
-void IRAM_ATTR ms_interrupt()
-{
-  flag_interrupt = 1;
-  timeout_interrupt = millis();
-}
-
-
-
 void setup() 
 {
-  pinMode(BTN,INPUT);
-  Serial.begin(115200);
+  /***        --- Initiate access point and configure static ip addresss as 06.14.22.24 ---         ***/
+
+  WiFi.mode(WIFI_AP);  
+  WiFi.softAP(ssid, password);
+  IPAddress IP = IPAddress (06, 14, 22, 24);
+  IPAddress NMask = IPAddress (255, 255, 255, 0);
+  IPAddress myIP = WiFi.softAPIP();
+  WiFi.softAPConfig(IP, IP, NMask);
+  pinMode(4,OUTPUT);
+  digitalWrite(4,0);
+
+/***          --- Initiate webserver  ---         ***/
+
+  server.on("/display", HTTP_GET, []() {
+    String message = "Missao: " + String(missao) + "\nEstado da Missao: " + String(flag);
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", message);
+  });
+
+  /* INITIALIZE ESP2SOTA LIBRARY */
+  ESP2SOTA.begin(&server);
+  server.begin();
+
+  Serial.begin(115200); 
   strip.begin();
   strip.fill(strip.Color(0, 0, 0), 0, strip.numPixels());
   //strip.setBrightness(127);
-  attachInterrupt(BTN,ms_interrupt,FALLING);
+
 }
 
 void loop() 
 {
+
   //tim = millis();
+  server.handleClient();
   
   CAN_ID = CAN1.RXpacketBegin();
   
   if(CAN_ID == 0x502)
   {
-    //CAN1.RXgetDLC();
+    CAN1.RXgetDLC();
 
 
     uint8_t CAN_DLC = CAN1.RXgetDLC();
@@ -150,6 +164,7 @@ void loop()
     strip.fill(strip.Color(0, 0, 0), 0, strip.numPixels());
     delay(50);
     strip.show();
+    flag = 1;
     break;
   }
   if(tim + 500 <= millis())
@@ -160,89 +175,6 @@ void loop()
     CAN1.TXpackettransmit();
     tim = millis();
   }
-
-  if(flag_interrupt == 1 && millis()>= timeout_interrupt + 100)
-  {
-    if(digitalRead(BTN) == 0)
-    {
-      missao++;
-      if(missao < 0 || missao > 6)
-      {
-        missao = 0;
-      }
-      flag_interrupt = 0;
-    }
-  }
-
-
-  switch (missao)
-  {
-  case 0:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 1:
-    digitalWrite(Accelaration, HIGH);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 2:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, HIGH);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 3:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, HIGH);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 4:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, HIGH);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 5:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, HIGH);
-    digitalWrite(Inspection, LOW);
-    break;
-  case 6:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, HIGH);
-    break;
-  default:
-    digitalWrite(Accelaration, LOW);
-    digitalWrite(Skidpad, LOW);
-    digitalWrite(Autocross, LOW);
-    digitalWrite(Trackdrive, LOW);
-    digitalWrite(EBS_test, LOW);
-    digitalWrite(Inspection, LOW);
-    break;
-  }
-  Serial.println(missao);
-  
+  //strip.fill(strip.Color(255, 0, 0), 0, strip.numPixels());
 }
 
